33a8702fc5092997a85df5dd068fa9a9474a147b
Rajan <rajan.chauhan18@imperial.ac.uk>
2020-08-25 11:27:19 +0100

    Initial Commit

---
+++ resources/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  // sticky navigation
+  $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky')
+    } else {
+      $('nav').removeClass('sticky')
+    }
+  }, {
+    offset: '70px;'
+  });
+
+
+  // scroll to point
+  $('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000)
+  })
+
+  $('.js--scroll-to-start').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
+  })
+
+});
